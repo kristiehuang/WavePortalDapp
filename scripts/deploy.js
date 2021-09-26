21da77837b30a24c6d6c9932b5be580c27243cc6
Kristie Huang <kristiehuang01@gmail.com>
2021-09-26 13:54:38 -0700

    Add script to deploy to local testnet

---
+++ scripts/deploy.js
@@ -0,0 +1,27 @@
+// Script to compile & deploy WavePortal.sol to actual testnet chain
+
+const main = async () => {
+    const [deployer] = await hre.ethers.getSigners();
+    const accountBalance = await deployer.getBalance();
+  
+    console.log('Deploying contracts with account: ', deployer.address);
+    console.log('Account balance: ', accountBalance.toString());
+  
+    // compile our contract & generate the necessary `artifacts` files we need
+    const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+    const waveContract = await waveContractFactory.deploy();
+  
+    console.log('WavePortal address: ', waveContract.address);
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
--- scripts/run.js
+++ scripts/run.js
@@ -1,11 +1,11 @@
-// Script to compile, deploy, then execute WavePortal.sol
+// Script to compile, deploy, then execute WavePortal.sol on temporary test network
 
 const main = async () => {
     const [owner, randoPerson] = await hre.ethers.getSigners();
 
     // compile our contract & generate the necessary `artifacts` files we need
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
-    // every time you deploy, Hardhat will create a local Ethereum network for us, but just for this contract
+    // every time you run this script, Hardhat will create a local Ethereum network for us, but just for this contract
     // Q: does hardhat handle migrations?
     const waveContract = await waveContractFactory.deploy();
     // wait until contract is deployed to our local blockchain
